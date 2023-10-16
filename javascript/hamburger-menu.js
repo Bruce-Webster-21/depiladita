5610e506ef1ed61e9008e78c2abf3aebfb8f14c2
Bruce Webster <brucewebster222@gmail.com>
2023-10-16 17:26:18 -0400

    Added side bar

--- javascript/hamburger-menu.js
+++ javascript/hamburger-menu.js
@@ -1,10 +1,42 @@
 const hamburger = document.getElementById("hamburger-menu");
 const hamburger1 = document.getElementById("hamburger-menu-1");
+const sidebarMenu = document.getElementById("sidebar-menu")
+const logo = document.getElementById('logo')
+const hamburgerItems = document.querySelectorAll(".hamburger-item")
+const navbar = document.getElementById("nav-bar")
+const body = document.getElementById("body")
   
 hamburger.addEventListener("click", function() {
   this.classList.toggle("open");
+  sidebarMenu.classList.toggle("show-sidebar")
+  body.classList.toggle("no-scroll")
+  logo.classList.toggle("invert")
+  hamburgerItems.forEach((item) => {
+    item.classList.toggle("black")
+  })
 });
 
 hamburger1.addEventListener("click", function() {
   this.classList.toggle("open");
+  sidebarMenu.classList.toggle("show-sidebar")
+  if (this.classList.contains('open')) (
+    navbar.style.boxShadow = 'none'
+  )
+  if (this.classList.contains('open') == false) {
+    navbar.style.boxShadow = '0px 1px 15px #00000059'
+  }
 });
+
+sidebarMenu.addEventListener("click", () => {
+  if (hamburger.classList.contains('open')) {
+    hamburger.classList.toggle("open")
+    logo.classList.toggle("invert")
+    hamburgerItems.forEach((item) => {
+      item.classList.toggle("black")
+    })
+  }
+  if (hamburger1.classList.contains('open')) {
+    hamburger1.classList.toggle("open")
+  }
+  sidebarMenu.classList.toggle("show-sidebar")
+})
