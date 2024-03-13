e47474e13f984aee302934309ffc7305623b1f0b
Bruce Webster <brucewebster222@gmail.com>
2024-03-13 15:03:59 -0400

    Added comments and tidied up

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,9 +1,11 @@
 const observer = new IntersectionObserver((entries) => {
+  // Iterate through each entry in the intersection observer's list
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
+      // If the observed element is intersecting, add the "show" class to it
       entry.target.classList.add("show");
     } else {
+      // If the observed element is not intersecting, remove the "show" class from it
       entry.target.classList.remove("show");
     }
   });
@@ -11,4 +13,5 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 
+// Iterate through each hidden element and observe them with the IntersectionObserver
 hiddenElements.forEach((element) => observer.observe(element));
--- javascript/hamburger-menu.js
+++ javascript/hamburger-menu.js
@@ -1,59 +1,64 @@
-const hamburger = document.getElementById("hamburger-menu");
-const hamburger1 = document.getElementById("hamburger-menu-1");
-const sidebarMenu = document.getElementById("sidebar-menu")
-const logo = document.getElementById('logo')
-const hamburgerItems = document.querySelectorAll(".hamburger-item")
-const navbar = document.getElementById("nav-bar")
-const body = document.getElementById("body")
-  
+const hamburger = document.getElementById("hamburger-menu"); // Cover image hamburger menu icon
+const hamburger1 = document.getElementById("hamburger-menu-1"); // Main Hamburger menu icon 
+const hamburgerItems = document.querySelectorAll(".hamburger-item");
+
+const sidebarMenu = document.getElementById("sidebar-menu");
+const logo = document.getElementById('logo');
+const navbar = document.getElementById("nav-bar");
+const body = document.getElementById("body");
+
+// Event listener for cover image hamburger menu
 hamburger.addEventListener("click", function() {
   this.classList.toggle("open");
-  sidebarMenu.classList.toggle("show-sidebar")
-  body.classList.toggle("no-scroll")
-  logo.classList.toggle("invert")
+  sidebarMenu.classList.toggle("show-sidebar");
+  body.classList.toggle("no-scroll");
+  logo.classList.toggle("invert");
   hamburgerItems.forEach((item) => {
-    item.classList.toggle("black")
-  })
+    item.classList.toggle("black");
+  });
 });
 
+// Event listener for the main hamburger menu
 hamburger1.addEventListener("click", function() {
   this.classList.toggle("open");
-  sidebarMenu.classList.toggle("show-sidebar")
-  body.classList.toggle("no-scroll")
+  sidebarMenu.classList.toggle("show-sidebar");
+  body.classList.toggle("no-scroll");
+
   if (this.classList.contains('open')) {
-    navbar.style.boxShadow = 'none'
-    navbar.style['-webkit-box-shadow'] = 'none'
-    navbar.style['-moz-box-shadow'] = 'none'
-  }
-  if (this.classList.contains('open') == false) {
-    navbar.style.boxShadow = '0px 1px 15px #00000059'
-    navbar.style['-webkit-box-shadow'] = '0px 1px 15px #00000059'
-    navbar.style['-moz-box-shadow'] = '0px 1px 15px #00000059'
+    navbar.style.boxShadow = 'none';
+    navbar.style['-webkit-box-shadow'] = 'none';
+    navbar.style['-moz-box-shadow'] = 'none';
+  } else {
+    navbar.style.boxShadow = '0px 1px 15px #00000059';
+    navbar.style['-webkit-box-shadow'] = '0px 1px 15px #00000059';
+    navbar.style['-moz-box-shadow'] = '0px 1px 15px #00000059';
   }
 });
 
+// Event listener for clicks on the sidebar menu
 sidebarMenu.addEventListener("click", () => {
   if (hamburger.classList.contains('open')) {
-    hamburger.classList.toggle("open")
-    logo.classList.toggle("invert")
+    hamburger.classList.toggle("open");
+    logo.classList.toggle("invert");
     hamburgerItems.forEach((item) => {
-      item.classList.toggle("black")
-    })
+      item.classList.toggle("black");
+    });
   }
-  if (hamburger1.classList.contains('open')) {
-    hamburger1.classList.toggle("open")
-  }
-  sidebarMenu.classList.toggle("show-sidebar")
-  body.classList.toggle("no-scroll")
 
   if (hamburger1.classList.contains('open')) {
-    navbar.style.boxShadow = 'none'
-    navbar.style['-webkit-box-shadow'] = 'none'
-    navbar.style['-moz-box-shadow'] = 'none'
+    hamburger1.classList.toggle("open");
   }
-  if (hamburger1.classList.contains('open') == false) {
-    navbar.style.boxShadow = '0px 1px 15px #00000059'
-    navbar.style['-webkit-box-shadow'] = '0px 1px 15px #00000059'
-    navbar.style['-moz-box-shadow'] = '0px 1px 15px #00000059'
+
+  sidebarMenu.classList.toggle("show-sidebar");
+  body.classList.toggle("no-scroll");
+
+  if (hamburger1.classList.contains('open')) {
+    navbar.style.boxShadow = 'none';
+    navbar.style['-webkit-box-shadow'] = 'none';
+    navbar.style['-moz-box-shadow'] = 'none';
+  } else {
+    navbar.style.boxShadow = '0px 1px 15px #00000059';
+    navbar.style['-webkit-box-shadow'] = '0px 1px 15px #00000059';
+    navbar.style['-moz-box-shadow'] = '0px 1px 15px #00000059';
   }
-})
+});
